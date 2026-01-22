2eec3e8f1d2cdcec7006e16672cf59699067f63c
Praveenkumar <fistotech04@gmail.com>
2026-01-22 15:42:17 +0530

    vital config update

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,27 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   optimizeDeps: {
     include: ["fabric"],
   },
   define: {
-    global: 'window', 
+    global: "window",
   },
   server: {
-    host: '0.0.0.0', // <-- IMPORTANT: listen on all interfaces for ngrok
-    port: 5173,      // <-- optional: specify the port you run ngrok on
+    host: "0.0.0.0",
+    port: 5173,
     allowedHosts: [
-      'https://hcxqp38j-5000.inc1.devtunnels.ms'
+      "hcxqp38j-5000.inc1.devtunnels.ms",
+      "fisto-idc-product.onrender.com",
     ],
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
-  }
+  },
 });
